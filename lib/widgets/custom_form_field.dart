55d3e6ea180ffb8827b1e48be80afdacac51e2be
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-19 20:22:25 -0300

    Validator and Keyboardtype
    Parameters for keyboardtype and validator to CustomFormField

--- lib/widgets/custom_form_field.dart
+++ lib/widgets/custom_form_field.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
 class CustomFormField extends StatelessWidget {
-  const CustomFormField({required this.controller, required this.labelText, this.padding, super.key});
+  const CustomFormField({
+    required this.controller,
+    required this.labelText,
+    this.padding,
+    this.keyboardType,
+    this.validator,
+    super.key,
+  });
 
   final String labelText;
   final TextEditingController controller;
   final EdgeInsets? padding;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding ?? const EdgeInsets.all(0),
-      child: TextField(
+      child: TextFormField(
         controller: controller,
+        keyboardType: keyboardType ?? TextInputType.text,
         decoration: InputDecoration(
           border: const OutlineInputBorder(),
           labelText: labelText,
         ),
+        validator: validator,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
       ),
     );
   }
