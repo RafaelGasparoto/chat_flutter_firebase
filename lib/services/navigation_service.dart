1b880ee14f5bcfaf85dbb32a7001d1431d98e28f
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-21 16:07:42 -0300

    Register page route

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,5 +1,6 @@
 import 'package:chat_flutter_firebase/pages/home/home_page.dart';
 import 'package:chat_flutter_firebase/pages/login/login_page.dart';
+import 'package:chat_flutter_firebase/pages/register/register_page.dart';
 import 'package:flutter/material.dart';
 
 class NavigationService {
@@ -10,6 +11,7 @@ class NavigationService {
   final Map<String, WidgetBuilder> routes = {
     '/login': (_) => const LoginPage(),
     '/home': (_) => const HomePage(),
+    '/register': (_) => const RegisterPage(),
   };
 
   void navigateToNamed(String routeName) => _navigatorKey.currentState!.pushNamed(routeName);
