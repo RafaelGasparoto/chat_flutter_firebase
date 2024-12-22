24635df38d1e34ab9aa0e4bb08cdfbc33cd88675
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-22 17:43:41 -0300

    Generate chat id

---
+++ lib/utils/generate_chat_id.dart
@@ -0,0 +1,6 @@
+String generateChatId({required String uid1, required String uid2}) {
+  List uids = [uid1, uid2];
+  uids.sort();
+  String chatId = uids.fold('', (id, uid) => '$id$uid');
+  return chatId;
+}
