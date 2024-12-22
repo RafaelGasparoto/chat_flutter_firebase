39c12df6d982be4496c546792bab57cbf1281dc7
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2024-12-22 17:43:30 -0300

    Navigate to chat page
    - Checks if the chats exists, if not, create a new chat with user and outer user uid

--- lib/pages/home/components/chat_list_tile.dart
+++ lib/pages/home/components/chat_list_tile.dart
@@ -1,19 +1,35 @@
 import 'package:chat_flutter_firebase/models/user.dart';
+import 'package:chat_flutter_firebase/pages/chat/chat_page.dart';
+import 'package:chat_flutter_firebase/services/auth_service.dart';
+import 'package:chat_flutter_firebase/services/database_service.dart';
 import 'package:chat_flutter_firebase/services/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 class ChatListTile extends StatelessWidget {
-  ChatListTile(this._user, {super.key});
-  
-  final NavigationService _navigationService = GetIt.instance.get<NavigationService>();
+  const ChatListTile(this._user, {super.key});
+
   final User _user;
 
   @override
   Widget build(BuildContext context) {
+    GetIt getIt = GetIt.instance;
+    final NavigationService navigationService = getIt.get<NavigationService>();
+    final DatabaseService databaseService = getIt.get<DatabaseService>();
+    final AuthService authService = getIt.get<AuthService>();
+
     return ListTile(
-      onTap: () {
-        _navigationService.navigateToNamed('/chat');
+      onTap: () async {
+        if (!await databaseService.checkChatExists(authService.user!.uid, _user.uid!)) {
+          databaseService.createChat(currentUserUid: authService.user!.uid, otherUserUid: _user.uid!);
+        }
+        navigationService.pushRoute(
+          MaterialPageRoute(
+            builder: (context) {
+              return ChatPage(otherUser: _user);
+            },
+          ),
+        );
       },
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       title: Text(_user.name!),
