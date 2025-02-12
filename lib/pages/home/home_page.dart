3fee2b459956de2662413a3cc2f2b1e45191e561
Rafael Gasparoto <rafaelgasparoto4@gmail.com>
2025-02-11 21:24:19 -0300

    Recover user profile picture in the home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_flutter_firebase/models/user.dart';
 import 'package:chat_flutter_firebase/pages/home/components/chat_list_tile.dart';
 import 'package:chat_flutter_firebase/services/auth_service.dart';
 import 'package:chat_flutter_firebase/services/current_user_service.dart';
@@ -19,7 +20,6 @@ class _HomePageState extends State<HomePage> {
   late final AuthService _authService;
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
-  late final CurrentUserService _currentUser;
   late final NotificationService _notificationService;
 
   @override
@@ -28,15 +28,11 @@ class _HomePageState extends State<HomePage> {
     _navigationService = GetIt.instance.get<NavigationService>();
     _databaseService = GetIt.instance.get<DatabaseService>();
     _notificationService = GetIt.instance.get<NotificationService>();
-    _getCurrentUser();
+    _notificationService.setFmcToken();
     super.initState();
   }
 
-  Future<void> _getCurrentUser() async {
-    _currentUser = getIt.get<CurrentUserService>();
-    _currentUser.user = await _databaseService.getUser(_authService.user!.uid);
-    _notificationService.setFmcToken();
-  }
+  Future<void> _getCurrentUser() async => await _databaseService.getUser(_authService.user!.uid);
 
   @override
   Widget build(BuildContext context) {
@@ -58,16 +54,39 @@ class _HomePageState extends State<HomePage> {
 
   AppBar _appBar() {
     return AppBar(
-      title: const Row(
-        children: [
-          Image(
-            image: AssetImage('assets/icon/chat-icon.png'),
-            height: 40,
-            color: Colors.white,
-          ),
-          Expanded(child: Text('Flutter Firebase Chat')),
-        ],
-      ),
+      title: FutureBuilder(
+          future: _getCurrentUser(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return const Text('Erro ao buscar usuário');
+            }
+
+            final currentUser = snapshot.data as User;
+
+            return GestureDetector(
+              onTap: () => _navigationService.pushNamed('/profile', arguments: currentUser),
+              child: Row(
+                children: [
+                  currentUser.profilePicture == null
+                      ? CircleAvatar(
+                          backgroundImage: const AssetImage('assets/user.png'),
+                          backgroundColor: Colors.grey.shade300,
+                        )
+                      : CircleAvatar(backgroundImage: NetworkImage(currentUser.profilePicture!)),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(currentUser.name ?? '', style: const TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
       actions: [
         IconButton(
           icon: const Icon(Icons.logout),
